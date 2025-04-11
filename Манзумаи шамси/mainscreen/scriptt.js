e6ac842233549ac82d3c3283b85b954876b31b19
PixelKiller0909 <nazarovabdulhaq@gmail.com>
2025-04-11 09:41:30 +0500

    Delete Манзумаи шамси directory

--- Манзумаи шамси/mainscreen/scriptt.js
+++
@@ -1,8 +0,0 @@
-for (let i = 0; i < 10; i++) {
-    const asteroid = document.createElement('div');
-    asteroid.className = 'asteroid';
-    asteroid.style.setProperty('--x', Math.random());
-    asteroid.style.animationDelay = `${Math.random() * 5}s`;
-    document.body.appendChild(asteroid);
-  }
-  
